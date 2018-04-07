3738bfdf0f0ddc4cbe74b226b3966562ef4e4883
Sarah Meysman <sarahmeysman@hotmail.com>
2018-04-07 11:58:40 +0200

    to top

--- assets/scripts/totop.js
+++ assets/scripts/totop.js
@@ -1,16 +1,12 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("to-top").style.display = "block";
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {   // If page is scrolled more than 50px
+        $('#to-top').fadeIn(200);    // Fade in the arrow
     } else {
-        document.getElementById("to-top").style.display = "none";
+        $('#to-top').fadeOut(200);   // Else fade out the arrow
     }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+});
+$('#to-top').click(function() {   // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0         // Scroll to top of body
+    }, 500);
+});
