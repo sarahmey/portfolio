31b228a220f0e06644a2f28d515910f2a06c8570
Sarah Meysman <sarahmeysman@hotmail.com>
2018-04-12 17:45:03 +0200

    kleine aanpassingen lightbox - toc

--- assets/scripts/lightbox.js
+++ assets/scripts/lightbox.js
@@ -41,14 +41,14 @@
   // Descriptions of all options available on the demo site:
   // http://lokeshdhakar.com/projects/lightbox2/index.html#options
   Lightbox.defaults = {
-    albumLabel: 'Image %1 of %2',
+    albumLabel: 'Afbeelding %1 van %2',
     alwaysShowNavOnTouchDevices: false,
     fadeDuration: 600,
     fitImagesInViewport: true,
     imageFadeDuration: 600,
     // maxWidth: 800,
     // maxHeight: 600,
-    positionFromTop: 50,
+    positionFromTop: 150,
     resizeDuration: 700,
     showImageNumberLabel: true,
     wrapAround: false,
